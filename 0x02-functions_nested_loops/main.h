5102e9aacaa6e09d8256be220f97e95e7b01014c
tparerenyatwa99 <tparerenyatwa99@gmail.com>
2023-02-21 15:42:19 -0800

    3-islower.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,7 @@
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-void _islower(int c);
+int _islower(int c);
 void _isalpha(int c);
 void print_sign(int n);
 void _abs(int);
