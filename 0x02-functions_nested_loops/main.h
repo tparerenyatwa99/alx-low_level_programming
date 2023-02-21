6e5f95a152c9917b9689fc8b6cdae963e9fbd6cf
tparerenyatwa99 <tparerenyatwa99@gmail.com>
2023-02-21 12:42:28 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void _islower(int c);
+void _isalpha(int c);
+void print_sign(int n);
+void _abs(int);
+void print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+
+#endif
