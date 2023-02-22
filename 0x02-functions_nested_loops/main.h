9eb6c64f38d9496e9f3df207e2689b041294660b
tparerenyatwa99 <tparerenyatwa99@gmail.com>
2023-02-21 17:49:24 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,7 +5,7 @@ int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-void _isalpha(int c);
+int _isalpha(int c);
 void print_sign(int n);
 void _abs(int);
 void print_last_digit(int);
